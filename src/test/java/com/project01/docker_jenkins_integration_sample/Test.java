0ed5a4c14b629802d53b1bd1ed1f0759d81447fe
mathewmanueljmg <mathewmanueljmg@mathewmanueljmg.internal.cloudapp.net>
2021-03-11 19:16:43 +0000

    Second Commit

---
+++ src/test/java/com/project01/docker_jenkins_integration_sample/Test.java
@@ -0,0 +1,11 @@
+package com.project01.docker_jenkins_integration_sample;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+
+	}
+
+}
